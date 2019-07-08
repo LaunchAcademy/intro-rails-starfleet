728ef1cf83d9bc39ab765439d917c385ff8db13e
Cassandra Newell <cassandra.newell@protonmail.com>
2019-07-08 16:52:00 -0400

    after form

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -6,4 +6,24 @@ class StarshipsController < ApplicationController
   def show
     @starship = Starship.find(params[:id])
   end
+
+  def new
+    @starship = Starship.new
+  end
+
+  def create
+    @starship = Starship.new(new_starship_params)
+
+    if !@starship.save
+      redirect_to @starship
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def new_starship_params
+    params.require("starship").permit(:name, :ship_class, :location)
+  end
 end
